d118f3783c91d0b990b0c07ccbb98239fff128fa
Aafaq Ahmad <malikafaq788@gmail.com>
2023-11-17 17:58:09 +0500

    profile updated

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,17 @@ import PopupSidebar from "./PopupSidebar/PopupSidebar";
 import Content from "./Content/Content";
 import Header from "./Header/Header";
 import MarketOverview from "./Charts/MarketOverview/MarketOverview";
+import AccountInformation from "./AccountInformation/AccountInformation";
+import MyAccount from "./MyAccount/MyAccount";
+import PrivacySecurity from "./PrivacySecurity/PrivacySecurity";
 
-export { Sidebar, PopupSidebar, Content, Header, MarketOverview };
+export {
+  Sidebar,
+  PopupSidebar,
+  Content,
+  Header,
+  MarketOverview,
+  AccountInformation,
+  MyAccount,
+  PrivacySecurity,
+};
--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,15 +1,13 @@
 import React from "react";
-import MyAccount from "../../components/MyAccount/MyAccount";
-import AccountInformation from "../../components/AccountInformation/AccountInformation";
-import PrivacySecurity from "../../components/PrivacySecurity/PrivacySecurity";
+import { AccountInformation, MyAccount, PrivacySecurity } from "../../components";
 
 const Profile = () => {
   const tabData = [{ label: "Tab 1" }, { label: "Tab 2" }, { label: "Tab 3" }];
   return (
     <div className="App">
-      {/* <MyAccount/> */}
-      {/* <AccountInformation/> */}
-      <PrivacySecurity/>
+      <MyAccount />
+      <AccountInformation />
+      <PrivacySecurity />
     </div>
   );
 };

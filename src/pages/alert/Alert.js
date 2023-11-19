d25ed0cb37c5127558d2bdd409eb8ff8813c3d06
Aafaq Ahmad <malikafaq788@gmail.com>
2023-11-19 22:53:43 +0500

    alert page updated

--- src/pages/alert/Alert.js
+++ src/pages/alert/Alert.js
@@ -50,23 +50,6 @@ const Alert = () => {
             </div>
           </div>
           <div className="timeRemaining">
-<<<<<<< HEAD
-            <h4>Last 7 days</h4>
-            <p>Place a buy trade on Cadbury at 12.7</p>
-            <p>Place a buy trade on Cadbury at 12.7</p>
-            <h4>Last 30 days</h4>
-            <p>Place a buy trade on Cadbury at 12.7</p>
-            <h4>Last 7 days</h4>
-            <p>Place a buy trade on Cadbury at 12.7</p>
-            <p>Place a buy trade on Cadbury at 12.7</p>
-            <h4>Last 30 days</h4>
-            <p>Place a buy trade on Cadbury at 12.7</p>
-            <h4>Last 7 days</h4>
-            <p>Place a buy trade on Cadbury at 12.7</p>
-            <p>Place a buy trade on Cadbury at 12.7</p>
-            <h4>Last 30 days</h4>
-            <p>Place a buy trade on Cadbury at 12.7</p>
-=======
             <p className="date">Last 7 days</p>
             <p className="item">Place a buy trade on Cadbury at 12.7</p>
             <p className="item">Place a buy trade on Cadbury at 12.7</p>
@@ -88,32 +71,10 @@ const Alert = () => {
             <p className="item">Place a buy trade on Cadbury at 12.7</p>
             <p className="item">Place a buy trade on Cadbury at 12.7</p>
             <p className="item">Place a buy trade on Cadbury at 12.7</p>
->>>>>>> dde0464ca44d8928ff585f67036b31498f253292
           </div>
         </div>
         <div className="alertLeftBottom">
           <h4>Alert</h4>
-<<<<<<< HEAD
-          <div className="alertRow">
-=======
-          {/* <div className="alertRow">
->>>>>>> dde0464ca44d8928ff585f67036b31498f253292
-            <div className="alertColumn1">
-              <h4>Symbol</h4>
-              <p>CAD</p>
-            </div>
-            <div className="alertColumn3">
-              <h4>Time</h4>
-              <p>5:00 pm</p>
-            </div>
-            <div className="alertColumn4">
-              <h4>HELLO</h4>
-              <img src={AlertIcon} alt="Alert" />
-            </div>
-<<<<<<< HEAD
-          </div>
-=======
-          </div> */}
           <table className="custom-table">
             <thead>
               <tr>
@@ -135,7 +96,6 @@ const Alert = () => {
               ))}
             </tbody>
           </table>
->>>>>>> dde0464ca44d8928ff585f67036b31498f253292
         </div>
       </div>
       <div className="alertRight">
@@ -156,52 +116,7 @@ const Alert = () => {
               ))}
             </ol>
           </div>
-<<<<<<< HEAD
-          <div className="alertRightText">
-            <p>
-              2. Lorem ipsum dolor sit amet consectetur. Ullamcorper vestibulum
-              semper id era.{" "}
-            </p>
-          </div>
-          <div className="alertRightText">
-            <p>
-              3. Lorem ipsum dolor sit amet consectetur. Ullamcorper vestibulum
-              semper id era.{" "}
-            </p>
-          </div>
-          <div className="alertRightText">
-            <p>
-              4. Lorem ipsum dolor sit amet consectetur. Ullamcorper vestibulum
-              semper id era.{" "}
-            </p>
-          </div>
-          <div className="alertRightText">
-            <p>
-              4. Lorem ipsum dolor sit amet consectetur. Ullamcorper vestibulum
-              semper id era.{" "}
-            </p>
-          </div>
-          <div className="alertRightText">
-            <p>
-              4. Lorem ipsum dolor sit amet consectetur. Ullamcorper vestibulum
-              semper id era.{" "}
-            </p>
-          </div>
-          <div className="alertRightText">
-            <p>
-              4. Lorem ipsum dolor sit amet consectetur. Ullamcorper vestibulum
-              semper id era.{" "}
-            </p>
-          </div>
-        </div>
-        <div className="aiBottom">
-          <div className="messageHere">
-            <input type="text" placeholder="Type message here..." />
-            <img src={MessageIcon} alt="Message icon" />
-          </div>
-=======
           <ChatInput />
->>>>>>> dde0464ca44d8928ff585f67036b31498f253292
         </div>
       </div>
     </div>

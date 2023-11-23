59ec50d1426b3d85e0900e04e791670c3bbcf479
Aafaq Ahmad <malikafaq788@gmail.com>
2023-11-24 02:13:45 +0500

    alert page scrollbar added

--- src/pages/alert/Alert.js
+++ src/pages/alert/Alert.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./alert.css";
 import editIcon from "../../assets/images/edit.svg";
 import deleteIcon from "../../assets/images/delete.svg";
@@ -39,10 +39,82 @@ const aiIntro = [
 ];
 
 const Alert = () => {
+  const [isLeftTopScrolling, setIsLeftTopScrolling] = useState(false);
+  const [isLeftBottomScrolling, setIsLeftBottomScrolling] = useState(false);
+  const [isAlertRight, setIsAlertRight] = useState(false);
+  const alertLeftTopRef = useRef(null);
+  const alertLeftBottomRef = useRef(null);
+  const alertRightRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsLeftTopScrolling(true);
+    };
+
+    const hideScrollbar = () => {
+      setIsLeftTopScrolling(false);
+    };
+
+    const alertLeftTopElement = alertLeftTopRef.current;
+
+    alertLeftTopElement.addEventListener("scroll", handleScroll);
+    alertLeftTopElement.addEventListener("mouseleave", hideScrollbar);
+
+    return () => {
+      alertLeftTopElement.removeEventListener("scroll", handleScroll);
+      alertLeftTopElement.removeEventListener("mouseleave", hideScrollbar);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsLeftBottomScrolling(true);
+    };
+
+    const hideScrollbar = () => {
+      setIsLeftBottomScrolling(false);
+    };
+
+    const alertLeftBottomElement = alertLeftBottomRef.current;
+
+    alertLeftBottomElement.addEventListener("scroll", handleScroll);
+    alertLeftBottomElement.addEventListener("mouseleave", hideScrollbar);
+
+    return () => {
+      alertLeftBottomElement.removeEventListener("scroll", handleScroll);
+      alertLeftBottomElement.removeEventListener("mouseleave", hideScrollbar);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsAlertRight(true);
+    };
+
+    const hideScrollbar = () => {
+      setIsAlertRight(false);
+    };
+
+    const alertRightElement = alertRightRef.current;
+
+    alertRightElement.addEventListener("scroll", handleScroll);
+    alertRightElement.addEventListener("mouseleave", hideScrollbar);
+
+    return () => {
+      alertRightElement.removeEventListener("scroll", handleScroll);
+      alertRightElement.removeEventListener("mouseleave", hideScrollbar);
+    };
+  }, []);
+
   return (
     <div className="alertCard">
       <div className="alertLeft">
-        <div className="alertLeftTop">
+        <div
+          className={`alertLeftTop ${
+            isLeftTopScrolling ? "scrolling" : "notscrolling"
+          }`}
+          ref={alertLeftTopRef}
+        >
           <div className="historyCard">
             <p>History</p>
             <div className="newChatButton">
@@ -73,7 +145,12 @@ const Alert = () => {
             <p className="item">Place a buy trade on Cadbury at 12.7</p>
           </div>
         </div>
-        <div className="alertLeftBottom">
+        <div
+          className={`alertLeftBottom ${
+            isLeftBottomScrolling ? "scrolling" : "notscrolling"
+          }`}
+          ref={alertLeftBottomRef}
+        >
           <h4>Alert</h4>
           <table className="custom-table">
             <thead>
@@ -100,7 +177,12 @@ const Alert = () => {
       </div>
       <div className="alertRight">
         <div className="alertRightCard">
-          <div className="alertRightTextCard">
+          <div
+            className={`alertRightTextCard ${
+              isAlertRight ? "scrolling" : "notscrolling"
+            }`}
+            ref={alertRightRef}
+          >
             <div className="alertRightHeader">
               <h4>AI Prompt</h4>
               <p>Create an alert using our powerful Ai tool</p>

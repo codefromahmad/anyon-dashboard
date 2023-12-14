c1b2401e5fbd683eadac653c62c64f1ee98117cd
Aafaq Ahmad <malikafaq788@gmail.com>
2023-12-15 01:56:05 +0500

    markets page done on mobile

--- src/pages/markets/Markets.js
+++ src/pages/markets/Markets.js
@@ -294,70 +294,187 @@ const Markets = ({ stocks }) => {
           </div>
         )}
         {stockSelected && (
-          <div className="mobileStockMarkets">
-            <div className="mobileAlertTop">
-              <div className="aiPromptButtons">
-                <Link
-                  to="/technicals"
-                  className={
-                    pathname === "/technicals"
-                      ? "active-item"
-                      : "nonactive-item"
-                  }
-                >
-                  <p>Technicals</p>
-                </Link>
-                <Link
-                  to="/markets"
-                  className={
-                    pathname === "/markets" ? "active-item" : "nonactive-item"
-                  }
-                >
-                  <p>Simple</p>
-                </Link>
-                <Link
-                  to="/marketsai"
-                  className={
-                    pathname === "/marketsai" ? "active-item" : "nonactive-item"
-                  }
-                >
-                  <p>AI</p>
-                </Link>
-              </div>
-            </div>
-            <div className="stockInfo">
-              <div className="stockDetail">
-                <img src={stock} alt="stock" />
-                <div className="nameAndDesc">
-                  <p className="name">BLS</p>
-                  <p className="description">Bilingual system</p>
+          <>
+            <div className="mobileStockMarkets">
+              <div className="mobileAlertTop">
+                <div className="aiPromptButtons">
+                  <Link
+                    to="/technicals"
+                    className={
+                      pathname === "/technicals"
+                        ? "active-item"
+                        : "nonactive-item"
+                    }
+                  >
+                    <p>Technicals</p>
+                  </Link>
+                  <Link
+                    to="/markets"
+                    className={
+                      pathname === "/markets" ? "active-item" : "nonactive-item"
+                    }
+                  >
+                    <p>Simple</p>
+                  </Link>
+                  <Link
+                    to="/marketsai"
+                    className={
+                      pathname === "/marketsai"
+                        ? "active-item"
+                        : "nonactive-item"
+                    }
+                  >
+                    <p>AI</p>
+                  </Link>
                 </div>
               </div>
-              <div>
-                <div className="marketsNoTextEquity">
-                  <div className="no-text">
-                    <div className="nTextMarkets">
-                      <p>N</p>
+              <div className="stockInfo">
+                <div className="stockDetail">
+                  <img src={stock} alt="stock" />
+                  <div className="nameAndDesc">
+                    <p className="name">BLS</p>
+                    <p className="description">Bilingual system</p>
+                  </div>
+                </div>
+                <div>
+                  <div className="marketsNoTextEquity">
+                    <div className="no-text">
+                      <div className="nTextMarkets">
+                        <p>N</p>
+                      </div>
+                      <div className="om-text">
+                        <p className="price">10.50</p>
+                      </div>
+                      <p className="percent">+2.5 (+5.0%)</p>
                     </div>
-                    <div className="om-text">
-                      <p className="price">10.50</p>
-                    </div>
-                    <p className="percent">+2.5 (+5.0%)</p>
                   </div>
                 </div>
               </div>
+              <BuySellLot
+                showPopup={showPopup}
+                handleDecrement={handleDecrement}
+                handleIncrement={handleIncrement}
+                value={value}
+              />
+              <MarketsTabs links={data} expand={false} />
+              <div className="mainAreaContainerMobile">
+                <MainAreaChart data={pData} trend={"up"} />
+              </div>
             </div>
-            <BuySellLot
-              showPopup={showPopup}
-              handleDecrement={handleDecrement}
-              handleIncrement={handleIncrement}
-              value={value}
-            />
-            <MarketsTabs links={data} expand={false} />
-            <div className="mainAreaContainerMobile">
-              <MainAreaChart data={pData} trend={"up"} />
-            </div>
-          </div>
+            {confirm ? (
+              <div className="rightMarket">
+                <div className="orderBookContainer">
+                  <div className="orderBookHeader">
+                    <p className="heading">Order Book</p>
+                  </div>
+                  <div className="orderBookChart">
+                    <OrderBookAsks />
+                    <OrderBookBids />
+                  </div>
+                </div>
+                <div className="bidAskContainer">
+                  <div className="bidAskHeadings">
+                    <p className="bidSize">Size</p>
+                    <p className="bidText">Bid</p>
+                    <p className="askText">Ask</p>
+                    <p className="askSize">Size</p>
+                  </div>
+                  {bidAskData.map((item, index) => (
+                    <div className="bidAskItem">
+                      <p className="bidSize">{item.bidSize}</p>
+                      <div className="bidBG">
+                        <p className="bidText">{item.bidPrice}</p>
+                      </div>
+                      <div className="askBG">
+                        <p className="askText">{item.askPrice}</p>
+                      </div>
+                      <p className="askSize">{item.askSize}</p>
+                    </div>
+                  ))}
+                </div>
+                <div className="orderBook">
+                  <div className="orderBookHeader">
+                    <p className="heading">Trade</p>
+                  </div>
+                  <div className="tradeContainer">
+                    <div>
+                      <div
+                        onClick={() => setActiveButton("buy")}
+                        className={`${
+                          activeButton === "buy"
+                            ? "buyButton"
+                            : "nonActiveButton"
+                        }`}
+                      >
+                        <p>Buy</p>
+                      </div>
+                      <div className="orderType">
+                        <p>Order Type</p>
+                        <div className="orderTypeSelect">
+                          <select
+                            id="Schedule"
+                            value={orderType}
+                            onChange={hanldeOrderType}
+                          >
+                            <option value="market">Market</option>
+                            <option value="limit">Limit</option>
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <div
+                        onClick={() => setActiveButton("sell")}
+                        className={`${
+                          activeButton === "sell"
+                            ? "sellButton"
+                            : "nonActiveButton"
+                        }`}
+                      >
+                        <p>Sell</p>
+                      </div>
+                      <div className="orderType">
+                        <p>Quantity</p>
+                        <div className="orderTypeSelect">
+                          <input type="number" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {orderType === "limit" && (
+                    <div className="timeInForce">
+                      <p>Limit Price</p>
+                      <div className="orderTypeSelect">
+                        <select id="Schedule">
+                          <option value="day">90</option>
+                          <option value="week">80</option>
+                        </select>
+                      </div>
+                    </div>
+                  )}
+                  <div className="timeInForce">
+                    <p>Time-in-Force</p>
+                    <div className="orderTypeSelect">
+                      <select id="Schedule">
+                        <option value="day">Day</option>
+                        <option value="week">Week</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="mainButton">
+                    <div
+                      className={`${
+                        activeButton === "sell" ? "sellButton" : "buyButton"
+                      }`}
+                    >
+                      <p>Buy</p>
+                    </div>
+                  </div>
+                </div>
+                <Positions />
+              </div>
+            ) : null}
+          </>
         )}
         {popup && (
           <BuySellPopUpOverlay
